f8321240fd667d3f9e3e68db56482751c926f664
rPortelas <remy.portelas@gmail.com>
2017-12-28 17:38:00 +0100

    loaded data now pickled to speed-up execution(see load_data()

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import torch as t
 import torch.nn as nn
 from torch.autograd import Function as F
 from torch.autograd import Variable
+import _pickle as pickle
+import os.path
 
 class baseDNN(nn.Module):
 
@@ -43,43 +45,76 @@ def computeAccuracy(pred, y):
     correctPredictions = t.sum(predInd == trueInd)
     return correctPredictions / float(len(y))
 
+#load data from given files and save it as tensors to speed up next executions
+def load_data(trainDataFile,testDataFile):
+    if not os.path.isfile('data_tensors.p'):
+        x_train, y_train = read_data(trainDataFile)
+        x_test, y_test = read_data(testDataFile)
+
+        nb_class = len(np.unique(y_test))
+        #one_hot encoding of labels
+        #y_train = t.FloatTensor(oneHot(y_train, nb_class))
+        #y_test = t.FloatTensor(oneHot(y_test, nb_class))
+        x_train = t.FloatTensor(x_train)
+        y_train = t.LongTensor(y_train)
+
+        x_test = t.FloatTensor(x_test)
+        y_test = t.LongTensor(y_test)
+
+        data = [x_train,y_train,x_test,y_test]
+        pickle.dump(data, open('data_tensors.p', 'wb'))
+        return data
+    else:
+        return pickle.load(open('data_tensors.p', 'rb'))
+
+
+'''
 def oneHot(x, nbClass):
     xOneHot = np.zeros((len(x), nbClass))
     xOneHot[np.arange(len(x)), x] = 1
     return xOneHot
-
-if __name__ == '__main__':      
-    print('loading data...',end='',flush=True)
+'''
+'''
+def evaluation(model,N,Xeval,Yeval):
+    loss = 0
+    acc = 0
+    #Nbatch = 2048
+    for k in range(N // Nbatch):
+        
+        indsBatch = range(k * Nbatch, (k+1) * Nbatch)
+        X = Variable(Xeval[indsBatch, :], requires_grad=False)
+        Y = Variable(Yeval[indsBatch, :], requires_grad=False)
+        
+        X,Y = X.cuda(), Y.cuda()
+        
+        Ytil = model(X)
+        _, Y_not_onehot = Y.max(1)
+        l, a = loss_accuracy(Ytil, Y_not_onehot, Y)
+        loss += l
+        acc += a
+        #print acc
+    return loss.cpu().data.numpy() / float(k+1), acc.cpu().data.numpy() / float(k+1)
+'''
+if __name__ == '__main__':
     trainDataFile = "mnist_rotation_new/mnist_all_rotation_normalized_float_train_valid.amat"
     testDataFile = "mnist_rotation_new/mnist_all_rotation_normalized_float_test.amat"
 
-    x_train, y_train = read_data(trainDataFile)
-    x_test, y_test = read_data(testDataFile)
-
-    nb_class = len(np.unique(y_test))
-    #one_hot encoding of labels
-    #y_train = t.FloatTensor(oneHot(y_train, nb_class))
-    #y_test = t.FloatTensor(oneHot(y_test, nb_class))
-    x_train = t.FloatTensor(x_train)
-    y_train = t.LongTensor(y_train)
-
-    x_test = t.FloatTensor(x_test)
-    y_test = t.LongTensor(y_test)
+    print('loading data...',end='',flush=True)
+    x_train,y_train,x_test,y_test = load_data(trainDataFile, testDataFile)
     
-
     #shuffle train and test data
     print('done',flush=True)
     print(y_train.shape)
     print(y_test.shape)
     print(x_train.shape)
-    print(y_train.shape)
+    print(x_train.shape)
 
     #hyperparameters
     learning_rate = 0.05
     batch_size = 32
     train_size = x_train.shape[0]
-    epochs_nb = 1
-    cuda = False
+    epochs_nb = 2
+    cuda = True
     #graph_step = batch_size * 100
 
     #WARNING - Task specific
@@ -102,9 +137,14 @@ if __name__ == '__main__':
 
     optim = t.optim.SGD(model.parameters(), lr=learning_rate)
 
-    #training part
+    #book keeping
     nbBatchTotal = 0
-    losses = []
+    train_losses = []
+    train_acc = []
+    test_losses = []
+    test_acc = []
+
+    #training
     for e in range(epochs_nb):
         for i in range(train_size // batch_size):
             #load batch
@@ -117,16 +157,16 @@ if __name__ == '__main__':
             y_til = model(x)
             #loss and backward
             l = loss(y_til,y)
-            losses.append(l.data[0])
+            train_losses.append(l.data[0])
             optim.zero_grad()
             l.backward()
             optim.step()
 
-            #if (i % graph_step) == 0:
-            plt.plot(losses)
-            plt.xlabel('nb mini-batch updates')
-            plt.ylabel('loss')
-            plt.show(block=False)
+        plt.plot(train_losses)
+        plt.xlabel('nb mini-batch updates')
+        plt.ylabel('loss')
+        plt.show(block=False)
+
     '''   
     plt.plot([j*batch_size for j in xrange(nbBatchTotal + nbEpochs)], lossData)
     plt.ylabel('evolution of Loss during training')
