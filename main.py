7c7710dccc53e3a4856aa6c5edc18601a1f55b32
rPortelas <remy.portelas@gmail.com>
2018-02-08 15:26:14 +0100

    code cleaning, removed accuracy plots

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Oct 14 11:00:43 2017
-
-@author: rportelas
+ICLR Reproducibility challenge 2018
+Code reproduction of https://arxiv.org/pdf/1708.01547.pdf
+@author: Octave Mariotti & Rémy Portelas
 """
 
 import numpy as np
@@ -17,11 +17,14 @@ import sklearn.metrics as metric
 import DEN as DEN_model
 
 
+"""
+DNN-STL is a base deep neural network trained for each task separately
+It will use t BaseDNN networks traned on each task
+"""
 class DNN_STL(nn.Module):
     def __init__(self,sizes,mu=0.1,cuda=False):
         self.models = [baseDNN([784,312,128],mu=mu,cuda=cuda) for i in range(10)]
         self.num_tasks = 1
-
 
     def add_task(self):
         self.num_tasks += 1
@@ -49,6 +52,10 @@ class DNN_STL(nn.Module):
     def param_norm(self, p=2):
         return self.models[self.num_tasks-1].param_norm(p)
 
+"""
+This class defines a simble deep neural network.
+It can use semantic drift regularization (DNN-L2 in paper) or l-2 regularization (DNN in paper)
+"""
 class baseDNN(nn.Module):
 
     def __init__(self,sizes,mu=0.1,cuda=False):
@@ -133,23 +140,15 @@ class baseDNN(nn.Module):
             #forward
             y_til = self.forward(x)[:,(self.num_tasks-1)]
             #loss and backward
-            #print(F.sigmoid(y_til))
             l += loss(F.sigmoid(y_til),y.float())/batch_size
         return l.data[0]
 
     def sparsity(self):
-        num = 0
-        denom = 0
-        for i, l in enumerate(self.parameters()):
-            num += (l == 0).float().sum().data[0]
-            prod = 1
-            for dim in l.size():
-                prod *= dim
-            denom += prod
-        return num/denom
+        return 0.0
 
     def sparsify_thres(self, tau=0.01):
         pass
+
 
 def read_data(fname):
     f = open(fname)
@@ -159,19 +158,15 @@ def read_data(fname):
     y = data[:, -1].astype(int)
     return x,y
 
-#load data from given files and save it as tensors to speed up next executions
+#load data from given files and save it as tensors to speed up next execution
 def load_data(trainDataFile,testDataFile):
     if not os.path.isfile('data_tensors.p'):
         x_train, y_train = read_data(trainDataFile)
         x_test, y_test = read_data(testDataFile)
-
         nb_class = len(np.unique(y_test))
-        #one_hot encoding of labels
-        #y_train = t.FloatTensor(oneHot(y_train, nb_class))
-        #y_test = t.FloatTensor(oneHot(y_test, nb_class))
+
         x_train = t.FloatTensor(x_train)
         y_train = t.LongTensor(y_train)
-
         x_test = t.FloatTensor(x_test)
         y_test = t.LongTensor(y_test)
 
@@ -181,14 +176,6 @@ def load_data(trainDataFile,testDataFile):
     else:
         return pickle.load(open('data_tensors.p', 'rb'))
 
-
-
-def oneHot(x, nbClass):
-    xOneHot = np.zeros((len(x), nbClass))
-    xOneHot[np.arange(len(x)), x] = 1
-    return xOneHot
-
-
 def computeAccuracy(y_out, y_true):
     #pred and y are n*1 numpy arrays
     preds = np.zeros(np.shape(y_true))
@@ -196,7 +183,10 @@ def computeAccuracy(y_out, y_true):
     accuracy = metric.accuracy_score(y_true,preds)
     return accuracy
 
-
+"""
+Evaluates given model on given data,
+RETURNS: loss, area under ROC curve (auroc) and accuracy
+"""
 def evaluation(model,loss,x_eval,y_eval,nb_class,use_cuda=False):
     eval_batch_size = 2048
     l = 0
@@ -223,7 +213,7 @@ def evaluation(model,loss,x_eval,y_eval,nb_class,use_cuda=False):
     acc = computeAccuracy(y_score,np_y_eval)
     return l/nb_iter,auroc,acc
 
-#evaluate our model for all learned tasks, after = learning is complete. y_eval is a 1D vector with values from 0 to task-1
+#evaluate our model for all learned tasks, after learning is complete. y_eval is a 1D vector with values from 0 to task-1
 def overall_offline_evaluation(model, loss, x_eval,y_eval,use_cuda=False):
     eval_batch_size = 2048
     nb_tasks = model.num_tasks - 1
@@ -243,7 +233,6 @@ def overall_offline_evaluation(model, loss, x_eval,y_eval,use_cuda=False):
 
         y_til = [model(x) for model in timestamped_models]
 
-#        y_til = model(x)
         for t_idx in range(nb_tasks):
             y_til_t = y_til[t_idx]
             out = F.sigmoid(y_til_t[:,t_idx])
@@ -262,7 +251,7 @@ def overall_offline_evaluation(model, loss, x_eval,y_eval,use_cuda=False):
     return accs,aurocs
 
 
-def plot_curves(data_lists,model_name,curve_type,x_axis='nb of epochs',save_plot=True,display_plot=False,savedir="./figures/",filename='lonely_plot',styles=None):
+def plot_curves(data_lists,model_name,curve_type,x_axis='nb of epochs',save_plot=True,display_plot=False,savedir="./figures/",filename='lonely_plot',styles=None,curve_names=['train','test']):
     #data_lists must contain [train,test] or [train] values
     fig = plt.figure()
     for i,values in enumerate(data_lists):
@@ -271,9 +260,9 @@ def plot_curves(data_lists,model_name,curve_type,x_axis='nb of epochs',save_plot
         else:
             plt.plot(range(1,len(values)+1), values,styles[i])
     if len(data_lists) == 2:
-        plt.legend(['train '+curve_type,'test '+curve_type], loc='upper right')
+        plt.legend([curve_names[0]+curve_type,curve_names[1]+curve_type], loc='upper right')
     else:#only train
-        plt.legend(['train '+curve_type], loc='upper right')
+        plt.legend([curve_names[1]+curve_type], loc='upper right')
     plt.xlabel(x_axis)
     plt.ylabel(curve_type)
     if save_plot:
@@ -283,8 +272,7 @@ def plot_curves(data_lists,model_name,curve_type,x_axis='nb of epochs',save_plot
 
 
 if __name__ == '__main__':
-    #trainDataFile = "mnist_rotation_new/mnist_all_rotation_normalized_float_train_valid.amat"
-    #testDataFile = "mnist_rotation_new/mnist_all_rotation_normalized_float_test.amat"
+    #datasets available here: http://www.iro.umontreal.ca/~lisa/twiki/bin/view.cgi/Public/MnistVariations
     #trainDataFile = "mnist/mnist_train.amat"
     #testDataFile = "mnist/mnist_test.amat"
     trainDataFile = "mnist_all_background_images_rotation_normalized_train_valid.amat"
@@ -303,10 +291,9 @@ if __name__ == '__main__':
     learning_rate = 0.01
     batch_size = 32
     train_size = x_train.shape[0]
-    epochs_nb = 200
+    epochs_nb = 2
     cuda = False
     verbose = True
-    #WARNING - Task specific
     input_dim = 784
     output_dim = 10
 
@@ -332,17 +319,13 @@ if __name__ == '__main__':
         model = model.cuda()
         loss = loss.cuda()
 
+    if model_type == 'DEN': 
+        print("model before training")
+        print(model)
 
-    #book keeping per task
-    train_losses = []
-    test_losses = []
-    test_accs = []
-    train_accs = []
     #overall book_keeping
     test_aurocs = []
     train_aurocs = []
-    all_train_accs = []
-    all_test_accs = []
     test_average_aurocs_task = []
 
     #training of binary model for each task from 1 to T
@@ -365,16 +348,9 @@ if __name__ == '__main__':
         if(model_type != "DEN" or model.num_tasks == 1):
             old_l = float('inf')
 
-            #print(model.sparsity())
             for e in range(epochs_nb):
-                #print('epoch '+str(e))
                 if model_type == "DEN":
                     l = model.batch_pass(x_train, task_y_train, loss, optimizer, mu=.01, reg_list=[model.param_norm], args_reg=[[1]])
-                    #Early stopping
-                    if(old_l - l < 0):
-                        print("First train:", e,"epochs")
-                        break
-                    old_l = l
                     model.sparsify_thres()
                 elif model_type == "DNN":
                     l = model.batch_pass(x_train, task_y_train, loss, optimizer, reg_list=[model.param_norm], args_reg=[[2]])
@@ -389,24 +365,6 @@ if __name__ == '__main__':
                     break
                 old_l = l
 
-                test_l,_,test_acc = evaluation(model, loss, x_test, task_y_test, 2, use_cuda=cuda)
-                train_l,_,train_acc = evaluation(model, loss, x_train, task_y_train, 2, use_cuda=cuda)
-                test_accs.append(test_acc)
-                train_accs.append(train_acc)
-                test_losses.append(test_l)
-                train_losses.append(train_l)
-
-            if verbose:
-                plot_curves([train_losses,test_losses],'DEN','loss', filename="loss_task"+str(model.num_tasks))
-                plot_curves([train_accs,test_accs],'DEN','accuracy', filename="acc_task"+str(model.num_tasks))
-
-            test_accs = []
-            train_accs = []
-            train_losses = []
-            test_losses = []
-#            print(model.sparsity())
-
-
         else:
             #Saving parameters for network split/duplication
             old_params_list = [Variable(w.data.clone(), requires_grad=False) for w in model.parameters()]
@@ -417,39 +375,27 @@ if __name__ == '__main__':
             #split
             model.duplicate(x_train, task_y_train, loss, optimizer, old_params_list, n_epochs=epochs_nb)
 
-
-
         #evaluation of auroc'score
-        _,test_auroc,test_acc = evaluation(model, loss, x_test, task_y_test, 2, use_cuda=cuda)
-        _,train_auroc,train_acc = evaluation(model, loss, x_train, task_y_train, 2, use_cuda=cuda)
+        _,test_auroc,_ = evaluation(model, loss, x_test, task_y_test, 2, use_cuda=cuda)
+        _,train_auroc,_ = evaluation(model, loss, x_train, task_y_train, 2, use_cuda=cuda)
         test_aurocs.append(test_auroc)
         train_aurocs.append(train_auroc)
-        all_test_accs.append(test_acc)
-        all_train_accs.append(train_acc)
-        print("sparsity: " + str(model.sparsity()))
-        print("train_auroc: " + str(train_auroc))
         print("test_auroc: " + str(test_auroc))
-        print("train_acc: " +  str(train_acc))
-        if model_type == "DEN": print(model)
         print("\n##################################\n")
 
         model.add_task()
-        accs_test,aurocs_test = overall_offline_evaluation(model, loss, x_test, y_test, use_cuda=cuda)
+        _,aurocs_test = overall_offline_evaluation(model, loss, x_test, y_test, use_cuda=cuda)
         test_average_aurocs_task.append(np.mean(aurocs_test))
 
-    #save paper results
-    pickle.dump(test_average_aurocs_task, open(str(model_type)+'_paper_eval_data.p', 'wb'))
+    if model_type == 'DEN': 
+        print("model after training")
+        print(model)
 
-
+    #online evaluation plot
     plot_curves([train_aurocs,test_aurocs],'DEN','auroc',x_axis='nb of tasks', filename="online_auroc",styles=['--rv','--bs'])
-    plot_curves([all_train_accs,all_test_accs],'DEN','accuracy',x_axis='nb of tasks', filename="online_accuracy")
 
     #offline evaluation for all tasks
-    accs_train,aurocs_train = overall_offline_evaluation(model, loss, x_train, y_train, use_cuda=cuda)
-    accs_test,aurocs_test = overall_offline_evaluation(model, loss, x_test, y_test, use_cuda=cuda)
-
+    _,aurocs_train = overall_offline_evaluation(model, loss, x_train, y_train, use_cuda=cuda)
+    _,aurocs_test = overall_offline_evaluation(model, loss, x_test, y_test, use_cuda=cuda)
     plot_curves([aurocs_train,aurocs_test],'DEN','auroc',x_axis='nb of tasks', filename="offline_auroc",styles=['--rv','--bs'])
-    plot_curves([accs_train,accs_test],'DEN','accuracy',x_axis='nb of tasks', filename="offline_accuracy")
-
-    #WARNING THIS IS NOT TRAIN DIS IS TEST
-    plot_curves([test_average_aurocs_task],'DEN','auroc',x_axis='nb of tasks', filename="paper eval (test values, not train)",styles=['--rv'])
+    plot_curves([test_average_aurocs_task],'DEN','auroc',x_axis='nb of tasks', filename="paper eval on test set",styles=['--rv'],curve_names=['test'])
